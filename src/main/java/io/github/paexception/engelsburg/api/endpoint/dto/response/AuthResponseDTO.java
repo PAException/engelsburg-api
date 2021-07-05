97c6f9c509d307d37883c659a47fc66da14436b3
PAException <35733278+PAException@users.noreply.github.com>
2021-07-05 21:35:29 +0200

    Added refreshTokens

--- src/main/java/io/github/paexception/engelsburg/api/endpoint/dto/response/AuthResponseDTO.java
+++ src/main/java/io/github/paexception/engelsburg/api/endpoint/dto/response/AuthResponseDTO.java
@@ -5,10 +5,11 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@AllArgsConstructor
 @NoArgsConstructor
-public class LoginResponseDTO {
+@AllArgsConstructor
+public class AuthResponseDTO {
 
+	private String token;
 	private String refreshToken;
 
 }
