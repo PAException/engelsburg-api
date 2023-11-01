d8069ba320f15cd7c77cbe6bbdc083d8bb3ef2b7
Paul Huerkamp <35733278+PAException@users.noreply.github.com>
2023-11-01 01:38:28 +0100

    Fixed substitutes

--- src/test/java/io/github/paexception/engelsburg/api/test/service/scheduled/SubstituteParseTest.java
+++ src/test/java/io/github/paexception/engelsburg/api/test/service/scheduled/SubstituteParseTest.java
@@ -2,6 +2,9 @@ package io.github.paexception.engelsburg.api.test.service.scheduled;
 
 import io.github.paexception.engelsburg.api.service.scheduled.SubstituteUpdateService;
 import org.junit.jupiter.api.Test;
+import io.github.paexception.engelsburg.api.endpoint.dto.SubstituteDTO;
+import java.sql.Date;
+import java.util.Objects;
 
 public class SubstituteParseTest {
 
@@ -10,4 +13,174 @@ public class SubstituteParseTest {
         new SubstituteUpdateService(null, null, null).updateSubstitutes();
     }
 
+    @Test
+    public void substituteDTOEqualTest() {
+        //Correct 5a - 10e
+        SubstituteDTO dto1 = new SubstituteDTO(
+                new Date(169879761),
+                "10c",
+                2,
+                "M",
+                "BSU",
+                "GAR",
+                "Vertretung",
+                "Mo-21.2. / 4",
+                "H301",
+                "Aufg. vorhanden"
+        );
+        SubstituteDTO dto2 = new SubstituteDTO(
+                new Date(169879761),
+                "10c",
+                2,
+                "D",
+                "GRB",
+                "KLE",
+                "Betreuung",
+                "asg",
+                "dshssdssh",
+                "dshgdhsen"
+        );
+
+        assert Objects.equals(dto1, dto2);
+
+        //Different lesson 5a - 10e
+        SubstituteDTO dto3 = new SubstituteDTO(
+                new Date(169879761),
+                "10a",
+                2,
+                "M",
+                "BSU",
+                "GAR",
+                "Vertretung",
+                "Mo-21.2. / 4",
+                "H301",
+                "Aufg. vorhanden"
+        );
+        SubstituteDTO dto4 = new SubstituteDTO(
+                new Date(169879761),
+                "10c",
+                3,
+                "D",
+                "GRB",
+                "KLE",
+                "Betreuung",
+                "asg",
+                "dshssdssh",
+                "dshgdhsen"
+        );
+
+        assert !Objects.equals(dto3, dto4);
+
+        //Different class
+        SubstituteDTO dto5 = new SubstituteDTO(
+                new Date(169879761),
+                "Q3",
+                2,
+                "M",
+                "BSU",
+                "GAR",
+                "Vertretung",
+                "Mo-21.2. / 4",
+                "H301",
+                "Aufg. vorhanden"
+        );
+        SubstituteDTO dto6 = new SubstituteDTO(
+                new Date(169879761),
+                "10c",
+                2,
+                "D",
+                "GRB",
+                "KLE",
+                "Betreuung",
+                "asg",
+                "dshssdssh",
+                "dshgdhsen"
+        );
+
+        assert !Objects.equals(dto5, dto6);
+
+        //Different date
+        SubstituteDTO dto7 = new SubstituteDTO(
+                new Date(169761),
+                "10c",
+                2,
+                "M",
+                "BSU",
+                "GAR",
+                "Vertretung",
+                "Mo-21.2. / 4",
+                "H301",
+                "Aufg. vorhanden"
+        );
+        SubstituteDTO dto8 = new SubstituteDTO(
+                new Date(169879761),
+                "10c",
+                2,
+                "D",
+                "GRB",
+                "KLE",
+                "Betreuung",
+                "asg",
+                "dshssdssh",
+                "dshgdhsen"
+        );
+
+        assert !Objects.equals(dto7, dto8);
+
+        //Different teacher E1 - Q4
+        SubstituteDTO dto9 = new SubstituteDTO(
+                new Date(169761),
+                "Q3",
+                2,
+                "M",
+                "BSU",
+                "GAR",
+                "Vertretung",
+                "Mo-21.2. / 4",
+                "H301",
+                "Aufg. vorhanden"
+        );
+        SubstituteDTO dto10 = new SubstituteDTO(
+                new Date(169879761),
+                "Q3",
+                2,
+                "D",
+                "GRB",
+                "KLE",
+                "Betreuung",
+                "asg",
+                "dshssdssh",
+                "dshgdhsen"
+        );
+
+        assert !Objects.equals(dto9, dto10);
+
+        //Correct teacher E1 - Q4
+        SubstituteDTO dto11 = new SubstituteDTO(
+                new Date(169761),
+                "Q3",
+                2,
+                "M",
+                "BSU",
+                "GAR",
+                "Vertretung",
+                "Mo-21.2. / 4",
+                "H301",
+                "Aufg. vorhanden"
+        );
+        SubstituteDTO dto12 = new SubstituteDTO(
+                new Date(169879761),
+                "Q3",
+                2,
+                "D",
+                "BSU",
+                "KLE",
+                "Betreuung",
+                "asg",
+                "dshssdssh",
+                "dshgdhsen"
+        );
+
+        assert !Objects.equals(dto11, dto12);
+    }
 }
